c274b4e487db890b00c3fcd06c3c9ae47e46f857
rajnandini-navsoft <Rajnandini.raut@navsoft.in>
2022-07-19 19:54:10 +0530

    multiple_table_data

--- customer/urls.py
+++ customer/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from .views import CustomerCreate,CustomerList,CustomerSingle,CustomerUpdate,ProductInsert,CategoryInsert,ProductUpdate
+from .views import CustomerCreate,CustomerList,CustomerSingle,CustomerUpdate,ProductInsert,CategoryInsert,ProductUpdate,MultiTabProCustInsert,MultiDataFetch
 
 
 urlpatterns = [
@@ -10,6 +10,11 @@ urlpatterns = [
     url(r'^update/(?P<pk>[0-9]+)/$', CustomerUpdate.as_view(), name='update-customer'),
     # path('delete/<int:pk>/', CustomerDelete.as_view(), name='delete-customer')
     url('category/insert/',CategoryInsert.as_view(),name='category-insert'),
+    
     url('product/create/',ProductInsert.as_view(),name='create-product'),
-    url(r'^product/update/(?P<pk>[0-9]+)/$',ProductUpdate.as_view(),name='update-product')
+    url(r'^product/update/(?P<pk>[0-9]+)/$',ProductUpdate.as_view(),name='update-product'),
+    url('pro-cust/insert/',MultiTabProCustInsert.as_view(),name='product-customer-data-insert'),
+    url('pro-cust/multiple_get_data/',MultiDataFetch.as_view(),name='multiple-data-details')
+    
+
 ]
--- customer/views.py
+++ customer/views.py
@@ -22,7 +22,7 @@ class CustomerCreate(generics.CreateAPIView):
 
 class CustomerList(generics.ListAPIView):
     def get(self,request, format=None):
-        print("hlllll")
+        # print("hlllll")
         query=CustomerAdd.objects.all()
         return_array=[]
         for each_customer in query:
@@ -171,6 +171,93 @@ class ProductUpdate(generics.UpdateAPIView):
             'data':product_update_display
         }
         return Response(return_arr)
+
+        #-------------------DATA INSERT IN MULTIPLE TABLE AT ONCE-------------------------------
+class MultiTabProCustInsert(generics.CreateAPIView):
+    def post(self,request,format=None,):
+        data=request.data
+        # store=[]
+        name = data['name']
+        slug = data['slug']
+        # image =data['image']
+        brand =data['brand']
+        url =data['url']
+        shipping = data['shipping']
+        description=data['description']
+        price=data['price']
+        category=data['category']
+        featured = data['featured']
+        active = data['active']
+
+        first_name = data['first_name']
+        middle_name = data["middle_name"]
+        last_name = data["last_name"]
+        mobile = data["mobile"]
+        email = data["email"]
+        address = data["address"]
+
+        product_details=Product.objects.create(name=name,slug=slug,brand=brand,url=url,shipping=shipping,description=description,price=price,category_id=category,featured=featured,active=active)
+        customer_details=CustomerAdd.objects.create(first_name=first_name,middle_name=middle_name,last_name=last_name,mobile=mobile,email=email,address=address)
+        product_insert_display=Product.objects.filter(id=product_details.id).values("name","slug","brand","url","shipping","description","price","category","featured","active").first()
+        customer_insert_display=CustomerAdd.objects.filter(id=customer_details.id).values("first_name","middle_name","last_name","mobile","email","address").first()
+        # store.append(product_insert_display)
+        return_arr={
+            "masg":"Both model data inserted successfully",
+            "product_data":product_insert_display,
+            "customer_data":customer_insert_display
+            
+        }
+        return Response(return_arr)
+
+
+class MultiDataFetch(generics.ListAPIView):
+    def get(self,request,format=None):
+        product_table_fetch=Product.objects.all()
+        customer_table_fetch=CustomerAdd.objects.all()
+        return_product_array=[]
+        return_customer_array=[]
+
+        for each_product in product_table_fetch:
+            each_array={
+                "name" : each_product.name,
+                "slug" : each_product.slug,
+                # image =each_product.image,
+                "brand" :each_product.brand,
+                "url" :each_product.url,
+                "shipping" :each_product.shipping,
+                "description":each_product.description,
+                "price":each_product.price,
+                "category":each_product.category_id,
+                "featured" : each_product.featured,
+                "active" : each_product.active
+            }
+            return_product_array.append(each_array)
+
+            
+                
+            
+        for each_customer in customer_table_fetch:
+            each_cust_arr={
+                "first_name":each_customer.first_name,
+                "middle_name":each_customer.middle_name,
+                "last_name":each_customer.last_name,
+                'mobile':each_customer.mobile,
+                "email":each_customer.email,
+                "address":each_customer.address
+
+,
+            }
+            return_customer_array.append(each_cust_arr)
+        return_arr={
+            "product_data":return_product_array,
+            "customer_data":return_customer_array
+        }
+        return Response(return_arr)
+
+            
+        
+    
+        
 
 
         
